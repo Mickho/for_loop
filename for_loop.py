cb320b46e78507895a85bfa376a9850a7adda6c7
Mickho <mick.my.ho@gmail.com>
2019-01-19 09:16:25 +0800

    add program

--- for_loop.py
+++ for_loop.py
@@ -1,5 +1,10 @@
 # for loop
 names = ['Allen','Tom', 'Mayday', 'JJ', 'Jolin', 'Jay', 'Jam']
-
 for name in names:
-	print('Hi', name)
+	print('Hi', name) #印出每一個names
+
+car = 'Ford'
+for cars in car:
+	print('car is :', cars) #印出car 裡面的字
+	print(cars in 'F') #判斷car 裡面有無F
+	print(cars in 'D') #判斷car 裡面有無D
